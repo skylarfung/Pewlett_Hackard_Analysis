945fa76a29684fa81132bbbca51f4fa4ff1a63aa
Skylar Fung <skylarfung@outlook.com>
2020-03-01 17:49:21 -0800

    Updated schema

--- schema.sql
+++ schema.sql
@@ -35,15 +35,6 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE slaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(40) NOT NULL,
@@ -62,3 +53,10 @@ CREATE TABLE dept_employees (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no)
 );
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
